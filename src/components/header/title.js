1e0bf86f24ca5ab72d7d1759ad10d447af6a3028
Abby Winn <abbywinn14@gmail.com>
2022-07-18 14:45:46 -0500

    no inline styling in heading

--- src/components/header/title.js
+++ src/components/header/title.js
@@ -4,15 +4,22 @@ import Typography from '@mui/material/Typography';
 
 export default function title(){
 
+    const boxStyle = {
+        width: '100%',
+        backgroundColor: '#3764A8',
+        mt: '20px',
+    };
+
+    const headingText = {
+        fontSize: '50px',
+        color: 'white',
+        fontWeight: 'bold',
+    };
 
     return (
         <Box
-        sx={{
-            width: '100%',
-            backgroundColor: '#3764A8',
-            mt: '20px',
-        }}>
-            <Typography align="center" variant="h2" color="white" fontWeight="bold">ADMINISTRATORS</Typography>
+        sx={boxStyle}>
+            <Typography align="center" sx={headingText}>ADMINISTRATORS</Typography>
         </Box>
     );
 }
